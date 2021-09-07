4880c65aaae7349851f451ff3abb3250fa1a00b3
Lucasalbertoaquino <48303532+Lucasalbertoaquino@users.noreply.github.com>
2021-09-07 01:58:32 -0300

    Add files via upload

---
+++ 06-listas/figuras/Ellipse.java
@@ -0,0 +1,39 @@
+package figuras;
+
+import java.awt.*;
+import java.awt.geom.Ellipse2D;
+import java.awt.geom.Ellipse2D.Double;
+
+public class Ellipse {
+    int x, y;
+    int w, h;
+    int rf,gf,bf;
+    int rl,gl,bl;
+
+    public Ellipse (int x, int y, int w, int h,int rf,int gf,int bf, int rl, int gl,int bl) {
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+        this.rf = rf;
+        this.gf = gf;
+        this.bf = bf;
+        this.rl = rl;
+        this.gl = gl;
+        this.bl = bl;
+    }
+
+    void print () {
+        System.out.format("Retangulo de tamanho (%d,%d) na posicao (%d,%d).\n",
+            this.w, this.h, this.x, this.y);
+    }
+
+    public void paint (Graphics g) {
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.setColor(new Color(this.rf,this.gf,this.bf));
+        g2d.fillOval(this.x,this.y,this.w,this.h); // resolve o fundo do  contorno
+        g2d.setColor(new Color(this.rl,this.gl,this.bl));
+        g2d.drawOval(this.x,this.y, this.w,this.h);//resolve o contorno
+        
+    }
+}
