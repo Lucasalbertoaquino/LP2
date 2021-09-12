841673e827915be9200931c38783b735cd9a80b2
Lucasalbertoaquino <48303532+Lucasalbertoaquino@users.noreply.github.com>
2021-09-12 00:16:32 -0300

    Add files via upload

--- Project/ListPackApp.java
+++ Project/ListPackApp.java
@@ -14,10 +14,7 @@ class ListPackApp{
     }
 }
 class ListPackFrame extends JFrame{
-    ArrayList<Rect> rs = new ArrayList<Rect>();
-    ArrayList<Ellipse> es = new ArrayList<Ellipse>();
-    ArrayList<Estrela> star = new ArrayList<Estrela>();
-    ArrayList<Triangulo> t = new ArrayList<Triangulo>();
+    ArrayList<Figure> figs = new ArrayList<Figure>();
     Random rand = new Random();// gera numeros aleatorios
 
     ListPackFrame(){
@@ -43,8 +40,8 @@ class ListPackFrame extends JFrame{
                         int rl = rand.nextInt(255);
                         int gl = rand.nextInt(255);
                         int bl = rand.nextInt(255);
-                        rs.add(new Rect(x,y,w,h,rf,gf,bf,rl,gl,bl));
-                        repaint();
+                        Rect r = new Rect(x,y,w,h,rf,gf,bf,rl,gl,bl);
+                        figs.add(r);
                     }
                     if((evt.getKeyChar() == 'e')||(evt.getKeyChar() == 'E')){
                         int x = rand.nextInt(350);//varia de 0 a 350
@@ -57,8 +54,7 @@ class ListPackFrame extends JFrame{
                         int rl = rand.nextInt(255);
                         int gl = rand.nextInt(255);
                         int bl = rand.nextInt(255);
-                        es.add(new Ellipse(x,y,w,h,rf,gf,bf,rl,gl,bl));
-                        repaint();
+                        figs.add(new Ellipse(x,y,w,h,rf,gf,bf,rl,gl,bl));
                     }
                     if((evt.getKeyChar() == 's')|| (evt.getKeyChar() =='S')){
                         int x = rand.nextInt(350);
@@ -71,8 +67,7 @@ class ListPackFrame extends JFrame{
                         int rl = rand.nextInt(255);
                         int gl = rand.nextInt(255);
                         int bl = rand.nextInt(255);
-                        star.add(new Estrela(x,x-50,x/2,x-70,x-100,x,x+100,x+70,x+150,x+50,y,y*2,y*2,y+180,y*4,y+240,y*4,y+180,y*2,y*2,rf,gf,bf,rl,gl,bl));
-                        repaint();
+                        figs.add(new Estrela(x,x-50,x/2,x-70,x-100,x,x+100,x+70,x+150,x+50,y,y*2,y*2,y+180,y*4,y+240,y*4,y+180,y*2,y*2,rf,gf,bf,rl,gl,bl));
                     }
                     else if((evt.getKeyChar() == 't')|| (evt.getKeyChar() == 'T')){
                         int x = rand.nextInt(350);//varia de 0 a 350
@@ -85,29 +80,20 @@ class ListPackFrame extends JFrame{
                         int rl = rand.nextInt(255);
                         int gl = rand.nextInt(255);
                         int bl = rand.nextInt(255);
-                        t.add(new Triangulo(x,y,w,h,x,y,rf,gf,bf,rl,gl,bl));
-                        repaint();
+                        figs.add(new Triangulo(x,y,w,h,x,y,rf,gf,bf,rl,gl,bl));
                     }
+                    repaint();
                 }
             }
         );
-        this.setTitle("lista de retangulos + elipse");
+        this.setTitle("lista de retangulos + elipse + triangulo + star");
         this.setSize(350,350);
     }
 
     public void paint (Graphics g){
         super.paint(g);
-        for (Rect r: this.rs){
-            r.paint(g);
-        }
-        for(Ellipse e: this.es){
-            e.paint(g);
-        }
-        for (Estrela s: this.star){
-            s.paint(g);
-        }
-        for(Triangulo t: this.t){
-            t.paint(g);
+        for(Figure fig: this.figs){
+            fig.paint(g);
         }
     }
 }
