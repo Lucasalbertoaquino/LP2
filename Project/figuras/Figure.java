9d6aa17b2c1861c8780649f6e1e201c9c7dcaef1
Lucasalbertoaquino <48303532+Lucasalbertoaquino@users.noreply.github.com>
2021-10-08 16:23:28 -0300

    Add files via upload

--- Project/figuras/Figure.java
+++ Project/figuras/Figure.java
@@ -5,8 +5,13 @@ import java.awt.Graphics;
 public abstract class Figure{
     public int x1, y1;
     public int w, h;
+
     int rf,gf,bf;
     int rl,gl,bl;
     public abstract void paint(Graphics g);
-    public abstract void print();
+    //public abstract void print();
+    public void drag(int dx, int dy){
+        this.x1 = dx;
+        this.y1 = dy;
+    }
 }
--- Project/figuras/Triangulo.java
+++ Project/figuras/Triangulo.java
@@ -8,10 +8,10 @@ public class Triangulo extends Figure{
 
     public Triangulo(int x1,int x2,int x3,int y1,int y2,int y3,int rf,int gf,int bf,int rl,int gl,int bl){
         super();
-        this.x1 = x1;
+        //this.x1 = x1;
         this.x2 = x2;
         this.x3 = x3;
-        this.y1 = y1;
+        //this.y1 = y1;
         this.y2 = y2;
         this.y3 = y3;
         this.rf = rf;
@@ -20,12 +20,22 @@ public class Triangulo extends Figure{
         this.rl = rl;
         this.gl = gl;
         this.bl = bl;
+
+        this.w = x3 - x2;
+        this.h = y3 - y1; 
+        this.x1 = x1;
+        this.y1 = y1;
+
     }
 
     public void paint(Graphics g){
         Graphics2D g2d = (Graphics2D) g;
+
         int [] x ={this.x1,this.x2,this.x3};
-        int [] y ={this.y1,this.y2,this.y3};
+        int [] y ={this.y1,this.y1,this.x3};
+
+        //int [] x ={this.x1,this.x2,this.x3};
+        //int [] y ={this.y1,this.y2,this.y3};
         g2d.setColor(new Color(this.rf,this.gf,this.bf));
         g2d.fillPolygon(x,y,3);
         g2d.setColor(new Color(this.rl,this.gl,this.bl));
