cbe906eec201f3ab4781b0a9e6d71db9e5f9ae83
Lucasalbertoaquino <48303532+Lucasalbertoaquino@users.noreply.github.com>
2021-10-08 16:23:04 -0300

    Add files via upload

--- Project/ListPackApp.java
+++ Project/ListPackApp.java
@@ -70,7 +70,7 @@ class ListPackFrame extends JFrame{
                         //figs.add(new Estrela(x,x-50,x/2,x-70,x-100,x,x+100,x+70,x+150,x+50,y,y*2,y*2,y+180,y*4,y+240,y*4,y+180,y*2,y*2,rf,gf,bf,rl,gl,bl));
                         figs.add(new Estrela(x,x-50,x/2,x-70,x-100,x,x+100,x+70,x+150,x+50,y/3,y-100,y-100,y-20,y+100,y+40,y,y-20,y-100,y-100,rf,gf,bf,rl,gl,bl));
                     }
-                    else if((evt.getKeyChar() == 't')|| (evt.getKeyChar() == 'T')){
+                    if((evt.getKeyChar() == 't')|| (evt.getKeyChar() == 'T')){
                         int x = rand.nextInt(350);//varia de 0 a 350
                         int y = rand.nextInt(350);
                         int w = rand.nextInt(50);
@@ -82,6 +82,12 @@ class ListPackFrame extends JFrame{
                         int gl = rand.nextInt(255);
                         int bl = rand.nextInt(255);
                         figs.add(new Triangulo(x,y,w,h,x,y,rf,gf,bf,rl,gl,bl));
+                        //figs.add(new Triangulo(100,10,200,100,150,180,rf,gf,bf,rl,gl,bl));
+
+
+                    }
+                    if((evt.getKeyChar() == 127)&&(focus != null)){
+                        figs.remove(figs.get(figs.indexOf(focus))); // remove o primeiro elemento
                     }
                     repaint();
                 }
@@ -98,8 +104,8 @@ class ListPackFrame extends JFrame{
                         int p2 = fig.y1 + fig.h;
                         
                         if(((mx >= fig.x1) &&(mx <= p1))&&((my >= fig.y1)&&(my <= p2))){
-                            //System.out.println("Acertei");
                             focus = fig;
+                            //System.out.println(fig);
                         }
                     }
                 }
@@ -113,9 +119,8 @@ class ListPackFrame extends JFrame{
                     for(Figure fig: figs){
                         int dx = evt.getX();
                         int dy = evt.getY();
-                        if(focus != null){
-                            fig.x1 = dx;
-                            fig.y1 = dy;
+                        if((focus != null)&&(focus == fig)){
+                            fig.drag(dx,dy);
                             repaint();
                         }
                     }
@@ -124,8 +129,7 @@ class ListPackFrame extends JFrame{
 
         );
 
-
-        this.setTitle("lista de retangulos + elipse + triangulo + star");
+        this.setTitle("Project");
         this.setSize(350,350);
     }
 
@@ -133,7 +137,7 @@ class ListPackFrame extends JFrame{
         super.paint(g);
         for(Figure fig: this.figs){
             fig.paint(g);
-            fig.print();
+            //fig.print();
         }
     }
 
